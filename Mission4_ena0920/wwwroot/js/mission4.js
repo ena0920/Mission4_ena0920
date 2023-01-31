462cb28fbc1a871017dbff15d5d75c0c02c91ae4
Emma Antonucci <emmanicole0920@gmail.com>
2023-01-30 18:41:51 -0700

    Add project files.

---
+++ Mission4_ena0920/wwwroot/js/mission4.js
@@ -0,0 +1,53 @@
+﻿$("#btnSend").click(function () {
+    let grade = parseInt($("#assignments").val())
+    let letterGrade = ""
+
+    grade += parseInt($("#groupProjects").val())
+
+    grade += parseInt($("#quizzes").val())
+
+    grade += parseInt($("#midtermExam").val())
+
+    grade += parseInt($("#finalExam").val())
+
+    grade += parseInt($("#intex").val())
+
+    if (grade >= 94) {
+        letterGrade = "A"
+    }
+    else if (grade >= 90) {
+        letterGrade = "A-"
+    }
+    else if (grade >= 87) {
+        letterGrade = "B+"
+    }
+    else if (grade >= 84) {
+        letterGrade = "B"
+    }
+    else if (grade >= 80) {
+        letterGrade = "B-"
+    }
+    else if (grade >= 77) {
+        letterGrade = "C+"
+    }
+    else if (grade >= 74) {
+        letterGrade = "C"
+    }
+    else if (grade >= 70) {
+        letterGrade = "C-"
+    }
+    else if (grade >= 67) {
+        letterGrade = "D+"
+    }
+    else if (grade >= 64) {
+        letterGrade = "D"
+    }
+    else if (grade >= 60) {
+        letterGrade = "D-"
+    }
+    else {
+        letterGrade = "E"
+    }
+
+    alert("Your numerical grade is: " + grade + "\nYour letter grade is: " + letterGrade)
+})
