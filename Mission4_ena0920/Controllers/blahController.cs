c258459e7d4fec1256dba3f9cb54e2c7d628e3a1
Emma Antonucci <emmanicole0920@gmail.com>
2023-02-09 19:52:21 -0700

    All done

---
+++ Mission4_ena0920/Controllers/blahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_ena0920.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_ena0920.Controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(gradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4_ena0920/Models/gradeCalculatorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_ena0920.Models
+{
+    public class gradeCalculatorModel
+    {
+        [Required]
+        public int Assignments { get; set; }
+
+        [Required]
+        public int GroupProjects { get; set; }
+
+        [Required]
+        public int Quizzes { get; set; }
+
+        [Required]
+        public int MidtermExam { get; set; }
+
+        [Required]
+        public int FinalExam { get; set; }
+
+        [Required]
+        public int Intex { get; set; }
+
+
+    }
+}
